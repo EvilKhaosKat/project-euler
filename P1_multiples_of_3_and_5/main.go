dcd90d906e3c8c0a84774632640f38c33f613845
EvilKhaosKat <evilkhaoskat@gmail.com>
2017-05-07 20:35:15 +0400

    Problem 1 solution

--- P1_multiples_of_3_and_5/main.go
+++ P1_multiples_of_3_and_5/main.go
@@ -1,4 +1,26 @@
 package main
 
+import "fmt"
+
+const MAX_NUMBER = 1000
+
 func main() {
+	multiples := []int{}
+
+	for i := 1; i < MAX_NUMBER; i++ {
+		if isMultipleOfThreeOrFive(i) {
+			multiples = append(multiples, i)
+		}
+	}
+
+	sum := 0
+	for _, value := range multiples {
+		sum = sum + value
+	}
+
+	fmt.Print(sum)
+}
+
+func isMultipleOfThreeOrFive(num int) bool {
+	return num % 3 == 0 || num % 5 == 0
 }
